c094b923dd6a810f5222d5a5e0a5639733e8ca6f
abhinit <abhinit@decision3.ai>
2022-06-17 08:36:24 +0000

    routes init

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+router.get('/todos', (req, res, next) => {
+  // get placeholder
+});
+router.post('/todos', (req, res, next) => {
+  // post placeholder
+});
+router.delete('/todos/:id', (req, res, next) => {
+  // delete placeholder
+});
+module.exports = router;
