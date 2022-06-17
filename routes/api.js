e75cffe91e77cc20c92a87f147edede68ca96bff
abhinit <abhinit.ambastha@gmail.com>
2022-06-17 16:48:40 +0800

    routes functional

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/todos', (req, res, next) => {
-  // get placeholder
+  res.json({
+    "_id": "0ax3247382hj2k3h42",
+    "name": "sne6593z_lidar"
+  });
 });
-router.post('/todos', (req, res, next) => {
-  // post placeholder
-});
-router.delete('/todos/:id', (req, res, next) => {
-  // delete placeholder
-});
+
 module.exports = router;
