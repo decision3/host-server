b992c0a2fe86b57c0a8f4dace67808278984c8f6
abhinit <abhinit.ambastha@gmail.com>
2022-06-17 16:54:16 +0800

    version endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const { exec } = require("child_process");
+
 const router = express.Router();
 
-router.get('/todos', (req, res, next) => {
-  res.json({
-    "_id": "0ax3247382hj2k3h42",
-    "name": "sne6593z_lidar"
-  });
+router.get('/version', (req, res, next) => {
+  exec("nitro-cli --version", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    res.json({
+      "version": stdout
+    });
+});
 });
 
 module.exports = router;
