3f84a4db6cb843c9a4d6f511e7402d0433bd95e7
abhinit <abhinit.ambastha@gmail.com>
2022-06-17 17:01:12 +0800

    build hello docker endpoint

--- routes/api.js
+++ routes/api.js
@@ -35,4 +35,21 @@ router.get('/describe', (req, res, next) => {
   });
 });
 
+router.get('/build/hello', (req, res, next) => {
+  exec("docker build /usr/share/nitro_enclaves/examples/hello -t hello", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    res.json({
+      "status": success,
+      "response": stdout
+    });
+  });
+});
+
 module.exports = router;
