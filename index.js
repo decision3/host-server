6e8700cd7ffc3552341c7c5ba0b9d888cd5ac534
abhinit <abhinit.ambastha@gmail.com>
2022-06-24 15:10:02 +0800

    router bugfix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = Router();
+const router = express.Router();
 
 // Importing routes
 const enclavesRoute = require('routes/enclaves.js');
