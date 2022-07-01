f06e0e42fa3d5d0bfcb0e45d71781634815a353d
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 23:13:09 +0800

    bugfix

--- routes/network.js
+++ routes/network.js
@@ -6,48 +6,38 @@ const { Console } = require('console');
 router.post('/start', (req, res, next) => {
 
     var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml';
-    // var processCount = 0;
+    var processCount = 0;
 
-    exec(command, (error, stdout, stderr) => {
-        console.log("starting the process");
+    exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
+        console.log("got some processes...");
+        processCount = parseInt(stdout);
+    });
+
+    if(processCount == 3) {
+        console.log("process is running");
         res
         .status(200)
         .json({
             "response": "Proxy is on"
         });
         return;
-    });
-
-    // exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
-    //     console.log("got some processes...");
-    //     processCount = parseInt(stdout);
-    // });
-
-    // if(processCount == 3) {
-    //     console.log("process is running");
-    //     res
-    //     .status(200)
-    //     .json({
-    //         "response": "Proxy is on"
-    //     });
-    //     return;
-    // } else if(processCount < 3){
-    //     console.log("process is not running");
-    //     console.log(command);
-    //     try {
-    //         exec(command, (error, stdout, stderr) => {
-    //             console.log("starting the process");
-    //             res
-    //             .status(200)
-    //             .json({
-    //                 "response": "Proxy is on"
-    //             });
-    //             return;
-    //         });
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    } else if(processCount < 3){
+        console.log("process is not running");
+        console.log(command);
+        try {
+            exec(command, (error, stdout, stderr) => {
+                console.log("starting the process");
+                res
+                .status(200)
+                .json({
+                    "response": "Proxy is on"
+                });
+                return;
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 });
 
 module.exports = router;
