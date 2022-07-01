64dedf3618727ed12229d91df5f14c6296000d98
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 23:15:45 +0800

    sync

--- routes/network.js
+++ routes/network.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const { Console } = require('console');
 
 router.post('/start', (req, res, next) => {
@@ -8,7 +8,7 @@ router.post('/start', (req, res, next) => {
     var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml';
     var processCount = 0;
 
-    exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
+    execSync('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
         console.log("got some processes...");
         processCount = parseInt(stdout);
     });
@@ -25,7 +25,7 @@ router.post('/start', (req, res, next) => {
         console.log("process is not running");
         console.log(command);
         try {
-            exec(command, (error, stdout, stderr) => {
+            execSync(command, (error, stdout, stderr) => {
                 console.log("starting the process");
                 res
                 .status(200)
