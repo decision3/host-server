8da6ba67cf4f6407217c5a579957dc6f87e27cc6
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 21:36:23 +0800

    process run check

--- routes/network.js
+++ routes/network.js
@@ -7,38 +7,22 @@ router.post('/start', (req, res, next) => {
   var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml &';
   
   exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
-      res
-      .status(200)
-      .json({
-        "response": stdout
-      });
+      if(parseInt(stdout) == 3) {
+        res
+        .status(200)
+        .json({
+          "response": "Proxy is on"
+        });
+      } else {
+        exec(command, (error, stdout, stderr) => {
+            res
+            .status(200)
+            .json({
+              "response": "Proxy is on"
+            });
+          });
+      }
   });
-
-//   exec(command, (error, stdout, stderr) => {
-//     if (error) {
-//       console.log(error);
-//       res
-//       .status(500)
-//       .json({
-//         "error": error.message
-//       });
-//       return;
-//     }
-//     if (stderr) {
-//       console.log(stderr);
-//       res
-//       .status(500)
-//       .json({
-//         "error": stderr
-//       });
-//       return;
-//     }
-//     res
-//     .status(200)
-//     .json({
-//       "response": "Proxy is on"
-//     });
-//   });
 
 });
 
