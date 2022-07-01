11fcf7de11fa3415725548014acf3eb42ad4491a
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 21:25:39 +0800

    test start

--- routes/network.js
+++ routes/network.js
@@ -6,31 +6,39 @@ router.post('/start', (req, res, next) => {
 
   var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml &';
   
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.log(error);
+  exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
       res
-      .status(500)
+      .status(200)
       .json({
-        "error": error.message
+        "response": stdout
       });
-      return;
-    }
-    if (stderr) {
-      console.log(stderr);
-      res
-      .status(500)
-      .json({
-        "error": stderr
-      });
-      return;
-    }
-    res
-    .status(200)
-    .json({
-      "response": "Proxy is on"
-    });
   });
+
+//   exec(command, (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//       res
+//       .status(500)
+//       .json({
+//         "error": error.message
+//       });
+//       return;
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//       res
+//       .status(500)
+//       .json({
+//         "error": stderr
+//       });
+//       return;
+//     }
+//     res
+//     .status(200)
+//     .json({
+//       "response": "Proxy is on"
+//     });
+//   });
 
 });
 
