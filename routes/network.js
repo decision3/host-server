85dfdf8d3d5c141115b1b351ec9001c8043adadf
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 23:18:25 +0800

    callback

--- routes/network.js
+++ routes/network.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
 const { Console } = require('console');
 
 router.post('/start', (req, res, next) => {
@@ -8,24 +8,22 @@ router.post('/start', (req, res, next) => {
     var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml';
     var processCount = 0;
 
-    execSync('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
+    exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
         console.log("got some processes...");
         processCount = parseInt(stdout);
-    });
 
-    if(processCount == 3) {
-        console.log("process is running");
-        res
-        .status(200)
-        .json({
-            "response": "Proxy is on"
-        });
-        return;
-    } else if(processCount < 3){
-        console.log("process is not running");
-        console.log(command);
-        try {
-            execSync(command, (error, stdout, stderr) => {
+        if(processCount == 3) {
+            console.log("process is running");
+            res
+            .status(200)
+            .json({
+                "response": "Proxy is on"
+            });
+            return;
+        } else if(processCount < 3){
+            console.log("process is not running");
+            console.log(command);
+            exec(command, (error, stdout, stderr) => {
                 console.log("starting the process");
                 res
                 .status(200)
@@ -34,10 +32,9 @@ router.post('/start', (req, res, next) => {
                 });
                 return;
             });
-        } catch (error) {
-            console.log(error);
         }
-    }
+        
+    });
 });
 
 module.exports = router;
