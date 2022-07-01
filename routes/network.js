11e4c99b98f5d204e28faddd0f2bc91b2f2f52d4
abhinit <abhinit.ambastha@gmail.com>
2022-07-01 21:52:33 +0800

    logging

--- routes/network.js
+++ routes/network.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const { exec } = require("child_process");
+const { Console } = require('console');
 
 router.post('/start', (req, res, next) => {
 
   var command = 'vsock-proxy 8001 ip-ranges.amazonaws.com 443 --config ~/host/enclave/nitro/vsock-proxy.yaml &';
   
   exec('ps -eaf | grep -c vsock', (error, stdout, stderr) => {
-      console.log(error,'\n', parseInt(stdout), '\n', stderr);
-      if(parseInt(stdout) == 3) {
+
+    if(parseInt(stdout) == 3) {
+        console.log("process is running");
         res
         .status(200)
         .json({
-          "response": "Proxy is on"
+            "response": "Proxy is on"
         });
         return;
-      } else if(parseInt(stdout) < 3){
+    } else if(parseInt(stdout) < 3){
+        console.log("process is not running");
         exec(command, (error, stdout, stderr) => {
             res
             .status(200)
             .json({
-              "response": "Proxy is on"
+                "response": "Proxy is on"
             });
         });
         return;
-      }
-      res
-      .status(500)
-      .json({
-        "response": "Network error"
-      });
+    }
+    console.log("process is running/not running/who cares");
+    res
+    .status(500)
+    .json({
+    "response": "Network error"
+    });
+
   });
 
 });
