b86f4e0d7527275b622650c68be44ae3e8e872f0
abhinit <abhinit.ambastha@gmail.com>
2022-07-02 16:14:39 +0800

    trim

--- routes/enclave.js
+++ routes/enclave.js
@@ -126,7 +126,7 @@ router.get('/', (req, res, next) => {
       res
       .status(200)
       .json({
-        "response": stdout
+        "response": stdout.trim()
       });
       return;
     }
