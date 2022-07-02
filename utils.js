157c5c0fb4925ee984cf22a3e07094ea7177cd28
abhinit <abhinit.ambastha@gmail.com>
2022-07-02 16:04:21 +0800

    empty json check

--- utils.js
+++ utils.js
@@ -2,8 +2,8 @@ const createCommand = (obj) => {
     var cmdArray = [];
     cmdArray.push(obj.entrypoint);
     const args = obj.arguments;
-    // for (var key of Object.keys(args)) cmdArray.push("--"+key+" "+args[key]);
-    console.log("Arguments:", args);
+    for (var key of Object.keys(args)) cmdArray.push("--"+key+" "+args[key]);
+    console.log(cmdArray);
     return cmdArray.join(" ");
 };
 
