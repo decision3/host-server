68a04ccb21035a5359b6d05b5b59fe8deb975154
abhinit <abhinit.ambastha@gmail.com>
2022-07-02 16:05:02 +0800

    clean

--- utils.js
+++ utils.js
@@ -3,7 +3,6 @@ const createCommand = (obj) => {
     cmdArray.push(obj.entrypoint);
     const args = obj.arguments;
     for (var key of Object.keys(args)) cmdArray.push("--"+key+" "+args[key]);
-    console.log(cmdArray);
     return cmdArray.join(" ");
 };
 
